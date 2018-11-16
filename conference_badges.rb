c1309c59ad02a457f5de3f3c8911bf68c2e3f02f
addai.5 <addai.5@buckeyemail.osu.edu>
2018-11-16 19:38:41 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  array = []
+  attendees.each do |name| 
+    array << badge_maker(name)
+  end
+  return array
+end
+
+def assign_rooms(attendees)
+  array = []
+  attendees.each {|name| array << "Hello, #{name}! You'll be assigned to room #{(attendees[name]).to_i+1}!"}
+  return array
+end
